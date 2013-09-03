df004b8484360f0b5c71f4e623c01bda1b1a69d6
rick00young <yyr168@gmail.com>
2013-09-03 21:05:53 +0800

    chagne

--- AStar/Astar.js
+++ AStar/Astar.js
@@ -1,8 +1,3 @@
-$(function() {
-	//alert("oooo");
-	create_div($("#box"), 100, 100);
-});
-
 
 function create_div($obj, width, height){
 	var w  = $obj.width();
@@ -13,10 +8,10 @@ function create_div($obj, width, height){
 	var num = w_n * h_n;
 	var div_array = [];
 	var string = '';
-	for(i  = 0; i < w_n; i++){
-		for(j = 0; j < h_n; j++){
+	for(i  = 0; i < h_n; i++){
+		for(j = 0; j < w_n; j++){
 			string  = '<div class="grid" style="width:'+(width - 2)+'px;height:'+(height - 2)+'px;" x="'+i+'" y="'+j+'"></div>';
-			$obj.append(string);		
+			$obj.append(string);
 		}
 	}
 }
@@ -37,11 +32,11 @@ Node.prototype = {
 }
 
 var Grid = function(cols, rows){
-	this.startNode = '';
-	this.endNode = '';
-	this.nodes = [];
-	this.numCols = cols;
-	this.cumRows = rows;
+	this._startNode = '';
+	this._endNode = '';
+	this._nodes = [];
+	this._numCols = cols;
+	this._cumRows = rows;
 	
 	for(var i = 0; i < this.numCols; i++){
 		this.nodes[i] = [];
@@ -53,18 +48,47 @@ var Grid = function(cols, rows){
 
 Grid.prototype = {
 	getNode:function(x, y){
-		return this.nodes[x][y];
+		return this._nodes[x][y];
 	},
 	setEndNode:function(x, y){
-		this.endNode = this.nodes[x][y];
+		this._endNode = this.nodes[x][y];
 	},
 	setStartNode:function(x, y){
-		this.startNode = this.nodes[x][y];
+		this._startNode = this.nodes[x][y];
 	},
 	setWalkable:function(x, y, bool){
-		this.nodes[x][y].walkable = bool;
+		this._nodes[x][y].walkable = bool;
+	},
+	getEndNode:function(){
+		return this._EndNode;
+	},
+	getStartNode:function(){
+		return this._startNode;
 	}
 }
+
+var GridTest = function(){
+	
+}
+
+var div_array = [];
+function generate_div_array(){
+	var $divs = $(".grid");
+	$divs.each(function(){
+		var x = $(this).attr('x') - 0;
+		var y = $(this).attr('y') - 0;
+		console.log(x + '-----' + y);
+		div_array[x] = [];
+		div_array[x][y] = $(this);
+	});
+	console.log(div_array.length + '----' + div_array[0].length);
+}
+
+$(function() {
+	//alert("oooo");
+	create_div($("#box"), 100, 100);
+	generate_div_array();
+});
 
 
 
