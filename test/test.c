a0dd99dc862067f3efbe146bcbca337c85e41b94
rick00young <yyr168@gmail.com>
2018-01-14 08:36:31 +0800

    up

--- test/test.c
+++ test/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../search/header.h"
+// #include "../search/header.h"
 /**
  * 冒泡需要比较n(n-1)/2,排序复杂度为O(n^2)
  */
@@ -8,13 +8,18 @@
 
 int main( int argc, char *argv[] ) /* 带参数形式 */
 {
-    int a[4] = {1,2,3,4};
-    for (int i = 0; i < 4; ++i)
-    {
-        printf("%d\n", a[i]);
-    }
-    a[3] = 8;
-    int j = -1;
-    printf("---%d\n", a[j]);
+    // int a[4] = {1,2,3,4};
+    // for (int i = 0; i < 4; ++i)
+    // {
+    //     printf("%d\n", a[i]);
+    // }
+    // a[3] = 8;
+    // int j = -1;
+    // printf("---%d\n", a[j]);
+
+    int i, j;
+    printf("请输入i,j:\n");
+    scanf("%d, %d", &i, &j);
+    printf("i is:%d, j is:%d\n", i, j);
     return 0;
 }
